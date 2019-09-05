f20d8051e23d136e0aebb08acfdc9e8d25e1f4f0
Pratyay Banerjee <pratyaybanerjee@Pratyays-MacBook-Pro.local>
2019-09-05 00:52:22 -0700

    fixing

--- run_model_abductive.sh
+++ run_model_abductive.sh
@@ -26,7 +26,9 @@ python merge_ir.py mnli_simple
 tar -zxvf trained_models/anli.tar
 
 
-python pytorch_transformers/models/hf_scorer.py --input_data_path  devf.jsonl   --eval_batch_size 8 --model_dir scratch/srmishr1/Sunpower/2015_swt_5e6_001_11 --bert_model bert-large-uncased-whole-word-masking --mcq_model bert-mcq-weighted-sum --tie_weights_weighted_sum --output_data_path .
+python pytorch_transformers/models/hf_scorer.py --input_data_path  devf.jsonl   --eval_batch_size 1 --model_dir scratch/srmishr1/Sunpower/2015_swt_5e6_001_11 --bert_model bert-large-uncased-whole-word-masking --mcq_model bert-mcq-weighted-sum --tie_weights_weighted_sum --output_data_path .
 
-mv predictions.txt /results/predictions.lst
+python fix_preds.py
+
+mv predictions2.txt /results/predictions.lst
 #python random_baseline.py --input-file /data/anli.jsonl --output-file /results/predictions.lst
