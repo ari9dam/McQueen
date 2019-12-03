71bf7fae8975aaced2442076ada1a580a5518a1c
swarooprm <swaroopranjanmishra@gmail.com>
2019-12-02 20:32:13 -0700

    Update ai2test2.py

--- scripts/Abductive/ai2test2.py
+++ scripts/Abductive/ai2test2.py
@@ -2,7 +2,7 @@ import jsonlines
 from tqdm import tqdm
 import json
 
-with jsonlines.open('dev.jsonl') as reader, jsonlines.open("dev_mcqueen_t.jsonl","w") as writer:
+with jsonlines.open('dev_orig.jsonl') as reader, jsonlines.open("dev_mcqueen_t.jsonl","w") as writer:
     for row in reader:
         outrow={
             "premises":[[row["obs1"],row["hyp1"]],[row["obs1"],row["hyp2"]]],
